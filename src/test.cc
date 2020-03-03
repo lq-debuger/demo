24c32be92f9bbc5fe194f9b602a79d332f1f3a21
lq-debuger <lfszliqiang@163.com>
2020-03-03 18:32:26 +0800

    test

--- src/test.cc
+++ src/test.cc
@@ -4,13 +4,14 @@
  * @Author: lq
  * @Date: 2020-03-03 17:51:56
  * @Descripttion: 
- * @LastEditTime: 2020-03-03 17:57:44
+ * @LastEditTime: 2020-03-03 18:31:59
  */
 
 #include<iostream>
 
 int main()
 {
+    int* p = nullptr;
     std::cout<< "hello" << std::endl;
     return 0;
 }
